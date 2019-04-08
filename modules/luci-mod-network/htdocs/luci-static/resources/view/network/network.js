slow-boat/luci

commit e57a52e9b73a7453b6465f15337de3e6891a37c1

luci-mod-network: fix page refresh after interface reconnect
Submitted-by: "taeasy" <88336@live.com>
[reword commit subject]
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-mod-network/htdocs/luci-static/resources/view/network/network.js
@@ -1,7 +1,7 @@
 function iface_reconnect(id) {
 	L.halt();
 	L.dom.content(document.getElementById(id + '-ifc-description'), E('em', _('Interface is reconnecting...')));
-	L.post(L.url('admin/network/iface_reconnect', id), L.run);
+	L.post(L.url('admin/network/iface_reconnect', id), null，L.run);
 }
 
 function iface_delete(ev) {

commit 11cb6eed26c65bfa52403b2e394413f0fd340106

luci-mod-network: fix bad unicode comma in network.js
Fixes: e57a52e9b ("luci-mod-network: fix page refresh after interface reconnect")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-mod-network/htdocs/luci-static/resources/view/network/network.js
@@ -1,7 +1,7 @@
 function iface_reconnect(id) {
 	L.halt();
 	L.dom.content(document.getElementById(id + '-ifc-description'), E('em', _('Interface is reconnecting...')));
-	L.post(L.url('admin/network/iface_reconnect', id), null，L.run);
+	L.post(L.url('admin/network/iface_reconnect', id), null, L.run);
 }
 
 function iface_delete(ev) {