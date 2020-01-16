slow-boat/luci

commit 1f03a07200f89ea514dd597ae0a8383ec08d870b

luci-app-mosquitto: drop tls1.0, add tls1.3
This was dropped in mosquitto 1.6.0.

Signed-off-by: Karl Palsson <karlp@etactica.com>

applications/luci-app-mosquitto/luasrc/model/cbi/mosquitto.lua
@@ -116,9 +116,9 @@ o = s:option(ListValue, "tls_version", "TLS Version",
     "Depends on your openssl version, empty to support all")
 o.optional = true
 o:value("", "Default")
-o:value("tlsv1")
 o:value("tlsv1.1")
 o:value("tlsv1.2")
+o:value("tlsv1.3")
 
 OptionalFlag(s, "require_certificate", "Require clients to present a certificate")
 OptionalFlag(s, "use_identity_as_username", "use_identity_as_username")

commit dd193232ac9136d8c86016d616e2ae37f78e9a76

luci-app-mosquitto: update tls versions for bridges as well
Earlier we updated the options list for tls versions for the broker's
own listeners, but we missed the list of tls versions used when
connecting to remote brokers.

Fixes: 1f03a07200f drop tls1.0, add tls1.3
Signed-off-by: Karl Palsson <karlp@etactica.com>

applications/luci-app-mosquitto/luasrc/model/cbi/mosquitto.lua
@@ -209,9 +209,9 @@ psk_key.validate = validate_psk_key
 b_tls_version = s:option(ListValue, "tls_version", _("TLS Version"),
     _("The remote broker must support the same version of TLS for the connection to succeed."))
 b_tls_version:value("", "Default")
-b_tls_version:value("tlsv1")
 b_tls_version:value("tlsv1.1")
 b_tls_version:value("tlsv1.2")
+b_tls_version:value("tlsv1.3")
 b_tls_version.optional = true
 
 o = s:option(Value, "cafile", _("Path to CA file"))