slow-boat/luci

commit 5fde99c1d6ec023415ad47cd350559bfdf2b76db

luci-base: extended network.js for check if new interface is createable
Signed-off-by: Helge Mader <ma@dev.tdt.de>

modules/luci-base/htdocs/luci-static/resources/network.js
@@ -2298,6 +2298,23 @@ Protocol = L.Class.extend(/** @lends LuCI.Network.Protocol.prototype */ {
 	},
 
 	/**
+	 * Check function for the protocol handler if a new interface is createable.
+	 *
+	 * This function should be overwritten by protocol specific subclasses.
+	 *
+	 * @abstract
+	 *
+	 * @param {string} ifname
+	 * The name of the interface to be created.
+	 *
+	 * @returns {Promise<null|error message>}
+	 * Returns `null` if new interface is createable, else returns (error) message.
+	 */
+	isCreateable: function(ifname) {
+		return Promise.resolve(null);
+	},
+
+	/**
 	 * Checks whether the protocol functionality is installed.
 	 *
 	 * This function exists for compatibility with old code, it always

commit 5bd89873aea73470520af3859732ce041cb3714e

luci-base: network.js: fix JSDoc syntax error
Fixes: 5fde99c1d ("luci-base: extended network.js for check if new interface is createable")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-base/htdocs/luci-static/resources/network.js
@@ -2312,7 +2312,7 @@ Protocol = L.Class.extend(/** @lends LuCI.Network.Protocol.prototype */ {
 	 * @param {string} ifname
 	 * The name of the interface to be created.
 	 *
-	 * @returns {Promise<null|error message>}
+	 * @returns {Promise<null|string>}
 	 * Returns `null` if new interface is createable, else returns (error) message.
 	 */
 	isCreateable: function(ifname) {