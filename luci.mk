slow-boat/luci

commit 8afc26f47bd5967cc444e0d11a9a408e02cbd32b

build: luci.mk: drop support for non-Git SCMs
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

luci.mk
@@ -60,20 +60,10 @@ LUCI_LC_ALIAS.zh_Hant=zh-tw
 PKG_NAME?=$(LUCI_NAME)
 
 PKG_VERSION?=$(if $(DUMP),x,$(strip $(shell \
-	if svn info >/dev/null 2>/dev/null; then \
-		revision="svn-r$$(LC_ALL=C svn info | sed -ne 's/^Revision: //p')"; \
-	elif git log -1 >/dev/null 2>/dev/null; then \
-		revision="svn-r$$(LC_ALL=C git log -1 | sed -ne 's/.*git-svn-id: .*@\([0-9]\+\) .*/\1/p')"; \
-		if [ "$$revision" = "svn-r" ]; then \
-			set -- $$(git log -1 --format="%ct %h" --abbrev=7); \
-			secs="$$(($$1 % 86400))"; \
-			yday="$$(date --utc --date="@$$1" "+%y.%j")"; \
-			revision="$$(printf 'git-%s.%05d-%s' "$$yday" "$$secs" "$$2")"; \
-		fi; \
-	else \
-		revision="unknown"; \
-	fi; \
-	echo "$$revision" \
+	set -- $$(git log -1 --format="%ct %h" --abbrev=7); \
+	secs="$$(($$1 % 86400))"; \
+	yday="$$(date --utc --date="@$$1" "+%y.%j")"; \
+	printf 'git-%s.%05d-%s' "$$yday" "$$secs" "$$2" \
 )))
 
 PKG_GITBRANCH?=$(if $(DUMP),x,$(strip $(shell \

commit c9d16644b72f23816a69b603f62daf9712fec5a3

build: gracefully handle non-Git source trees
Fixes: #3911
Fixes: 8afc26f47 ("build: luci.mk: drop support for non-Git SCMs")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

luci.mk
@@ -59,19 +59,30 @@ LUCI_LC_ALIAS.zh_Hant=zh-tw
 
 PKG_NAME?=$(LUCI_NAME)
 
-PKG_PO_VERSION?=$(if $(DUMP),x,$(strip $(shell \
-	set -- $$(git log -1 --format="%ct %h" --abbrev=7 -- po); \
-	secs="$$(($$1 % 86400))"; \
-	yday="$$(date --utc --date="@$$1" "+%y.%j")"; \
-	printf 'git-%s.%05d-%s' "$$yday" "$$secs" "$$2" \
-)))
 
-PKG_SRC_VERSION?=$(if $(DUMP),x,$(strip $(shell \
-	set -- $$(git log -1 --format="%ct %h" --abbrev=7 -- . ':(exclude)po'); \
-	secs="$$(($$1 % 86400))"; \
-	yday="$$(date --utc --date="@$$1" "+%y.%j")"; \
-	printf 'git-%s.%05d-%s' "$$yday" "$$secs" "$$2" \
-)))
+# 1: everything expect po subdir or only po subdir
+define findrev
+  $(shell \
+    if git log -1 >/dev/null 2>/dev/null; then \
+      set -- $$(git log -1 --format="%ct %h" --abbrev=7 -- '$(if $(1),:(exclude))po'); \
+      secs="$$(($$1 % 86400))"; \
+      yday="$$(date --utc --date="@$$1" "+%y.%j")"; \
+      printf 'git-%s.%05d-%s' "$$yday" "$$secs" "$$2"; \
+    else \
+      ts=$$(find . -type f $(if $(1),-not) -path './po/*' -printf '%T@\n' 2>/dev/null | sort -rn | head -n1 | cut -d. -f1); \
+      if [ -n "$$ts" ]; then \
+        secs="$$(($$ts % 86400))"; \
+        date="$$(date --utc --date="@$$ts" "+%y%m%d")"; \
+        printf '%s.%05d' "$$date" "$$secs"; \
+      else \
+        echo "unknown"; \
+      fi; \
+    fi \
+  )
+endef
+
+PKG_PO_VERSION?=$(if $(DUMP),x,$(strip $(call findrev)))
+PKG_SRC_VERSION?=$(if $(DUMP),x,$(strip $(call findrev,1)))
 
 PKG_GITBRANCH?=$(if $(DUMP),x,$(strip $(shell \
 	variant="LuCI"; \