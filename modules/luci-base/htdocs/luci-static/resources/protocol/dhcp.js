slow-boat/luci

commit 6a263b1efee65153b02adf7b8fb47da1604b5e7d

luci-base: add default value options dhcp hostname
Signed-off-by: Florian Eckert <fe@dev.tdt.de>

modules/luci-base/htdocs/luci-static/resources/protocol/dhcp.js
@@ -20,6 +20,9 @@ return network.registerProtocol('dhcp', {
 		var dev = this.getL2Device() || this.getDevice(), o;
 
 		o = s.taboption('general', form.Value, 'hostname', _('Hostname to send when requesting DHCP'));
+		o.default = '';
+		o.value('', 'Send the hostname of this device');
+		o.value('*', 'Do not send a hostname');
 		o.datatype    = 'or(hostname, "*")';
 		o.load = function(section_id) {
 			return callFileRead('/proc/sys/kernel/hostname').then(L.bind(function(hostname) {

commit 72bb7fd2a290af8aa660c84d177e3570fec86071

luci-base: make translatable default values for dhcp hostname
We must be able to translate these values.

Fixes: 6a263b1ef ("luci-base: add default value options dhcp hostname")
Signed-off-by: Anton Kikin <a.kikin@tano-systems.com>

modules/luci-base/htdocs/luci-static/resources/protocol/dhcp.js
@@ -21,8 +21,8 @@ return network.registerProtocol('dhcp', {
 
 		o = s.taboption('general', form.Value, 'hostname', _('Hostname to send when requesting DHCP'));
 		o.default = '';
-		o.value('', 'Send the hostname of this device');
-		o.value('*', 'Do not send a hostname');
+		o.value('', _('Send the hostname of this device'));
+		o.value('*', _('Do not send a hostname'));
 		o.datatype    = 'or(hostname, "*")';
 		o.load = function(section_id) {
 			return callFileRead('/proc/sys/kernel/hostname').then(L.bind(function(hostname) {