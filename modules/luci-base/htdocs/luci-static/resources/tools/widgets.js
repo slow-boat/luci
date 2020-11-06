slow-boat/luci

commit 3fa0702a5fc843212de757eb5e2698922188e11f

luci-base: tools.widget.NetworkSelect: disallow invalid network names
Fixes: #4523, #4573
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-base/htdocs/luci-static/resources/tools/widgets.js
@@ -393,6 +393,7 @@ var CBINetworkSelect = form.ListValue.extend({
 			select_placeholder: E('em', _('unspecified')),
 			display_items: this.display_size || this.size || 3,
 			dropdown_items: this.dropdown_size || this.size || 5,
+			datatype: 'uciname',
 			validate: L.bind(this.validate, this, section_id),
 			create: !this.nocreate,
 			create_markup: '' +

commit 124d686f1e4d536cbf70a768813ec105c8488bd5

luci-base: tools.widgets.NetworkSelect: properly validate multi select
Fixes: 3fa0702a5 ("luci-base: tools.widget.NetworkSelect: disallow invalid network names")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-base/htdocs/luci-static/resources/tools/widgets.js
@@ -393,7 +393,7 @@ var CBINetworkSelect = form.ListValue.extend({
 			select_placeholder: E('em', _('unspecified')),
 			display_items: this.display_size || this.size || 3,
 			dropdown_items: this.dropdown_size || this.size || 5,
-			datatype: 'uciname',
+			datatype: this.multiple ? 'list(uciname)' : 'uciname',
 			validate: L.bind(this.validate, this, section_id),
 			create: !this.nocreate,
 			create_markup: '' +