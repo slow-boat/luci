slow-boat/luci

commit c6e9631c01c8ba8cbc346a918f400ba959d97b9a

luci-mod-network: drop support for *editing* legacy bridges
The old way of defining bridge (L2) as part of interface (L3) is
deprecated. All such configs should be migrated to define bridge as L3
UCI section type "device".

Signed-off-by: Rafał Miłecki <rafal@milecki.pl>

modules/luci-mod-network/htdocs/luci-static/resources/view/network/interfaces.js
@@ -405,6 +405,12 @@ return view.extend({
 				}, this);
 				o.write = function() {};
 
+				o = s.taboption('general', widgets.DeviceSelect, 'ifname', _('Device'));
+				o.nobridges = false;
+				o.noaliases = false;
+				o.optional = false;
+				o.network = ifc.getName();
+
 				proto_select = s.taboption('general', form.ListValue, 'proto', _('Protocol'));
 				proto_select.modalonly = true;
 
@@ -677,7 +683,6 @@ return view.extend({
 				}
 
 				ifc.renderFormOptions(s);
-				nettools.addDeviceOptions(s, null, true);
 
 				// Common interface options
 				o = nettools.replaceOption(s, 'advanced', form.Flag, 'defaultroute', _('Use default gateway'), _('If unchecked, no default route is configured'));

commit 47c9be91b09ab16749f963b0b19f90913795fe9b

luci-mod-network: don't hide "Device" on protocol change
Device ("ifname" UCI option) doesn't depend on protocol so there is no
need to hide / reset it on protocol change.

While at it drop names of two removed inputs (dead code).

Fixes: c6e9631c01c8 ("luci-mod-network: drop support for *editing* legacy bridges")
Signed-off-by: Rafał Miłecki <rafal@milecki.pl>

modules/luci-mod-network/htdocs/luci-static/resources/view/network/interfaces.js
@@ -826,6 +826,7 @@ return view.extend({
 					o = s.children[i];
 
 					switch (o.option) {
+					case 'ifname':
 					case 'proto':
 					case 'auto':
 					case '_dhcp':
@@ -834,8 +835,6 @@ return view.extend({
 					case '_ifacestat_modal':
 						continue;
 
-					case 'ifname_multi':
-					case 'ifname_single':
 					case 'igmp_snooping':
 					case 'stp':
 					case 'type':