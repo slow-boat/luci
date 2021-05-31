slow-boat/luci

commit ed49bbb93fe9e3dca84e2e26b2dcc685a2559c87

luci-mod-network: drop support for *adding* legacy bridges
The old way of defining bridge (L2) as part of interface (L3) is
deprecated. Don't support *adding* interfaces like that.

Support for *editing* legacy bridges is kept for now for compatibility
with existing legacy setups.

Signed-off-by: Rafał Miłecki <rafal@milecki.pl>

modules/luci-mod-network/htdocs/luci-static/resources/view/network/interfaces.js
@@ -831,7 +831,7 @@ return view.extend({
 			var m2 = new form.Map('network'),
 			    s2 = m2.section(form.NamedSection, '_new_'),
 			    protocols = network.getProtocols(),
-			    proto, name, bridge, ifname_single, ifname_multi;
+			    proto, name, ifname;
 
 			protocols.sort(function(a, b) {
 				return a.getProtocol() > b.getProtocol();
@@ -861,33 +861,15 @@ return view.extend({
 				return true;
 			};
 
+			ifname = s2.option(widgets.DeviceSelect, 'ifname', _('Device'));
+			ifname.noaliases = false;
+			ifname.optional = false;
+
 			proto = s2.option(form.ListValue, 'proto', _('Protocol'));
 			proto.validate = name.validate;
 
-			bridge = s2.option(form.Flag, 'type', _('Bridge interfaces'), _('Creates a bridge over specified interface(s)'));
-			bridge.modalonly = true;
-			bridge.disabled = '';
-			bridge.enabled = 'bridge';
-
-			ifname_single = s2.option(widgets.DeviceSelect, 'ifname_single', _('Interface'));
-			ifname_single.noaliases = false;
-			ifname_single.optional = false;
-
-			ifname_multi = s2.option(widgets.DeviceSelect, 'ifname_multi', _('Interface'));
-			ifname_multi.nobridges = true;
-			ifname_multi.noaliases = true;
-			ifname_multi.multiple = true;
-			ifname_multi.optional = true;
-			ifname_multi.display_size = 6;
-
 			for (var i = 0; i < protocols.length; i++) {
 				proto.value(protocols[i].getProtocol(), protocols[i].getI18n());
-
-				if (!protocols[i].isVirtual()) {
-					bridge.depends({ proto: protocols[i].getProtocol() });
-					ifname_single.depends({ type: '', proto: protocols[i].getProtocol() });
-					ifname_multi.depends({ type: 'bridge', proto: protocols[i].getProtocol() });
-				}
 			}
 
 			m2.render().then(L.bind(function(nodes) {
@@ -920,16 +902,7 @@ return view.extend({
 										var section_id = uci.add('network', 'interface', nameval);
 
 										protoclass.set('proto', protoval);
-
-										if (ifname_single.isActive('_new_')) {
-											protoclass.addDevice(ifname_single.formvalue('_new_'));
-										}
-										else if (ifname_multi.isActive('_new_')) {
-											protoclass.set('type', 'bridge');
-											L.toArray(ifname_multi.formvalue('_new_')).map(function(dev) {
-												protoclass.addDevice(dev);
-											});
-										}
+										protoclass.addDevice(ifname.formvalue('_new_'));
 
 										m.children[0].addedSection = section_id;
 									}).then(L.bind(m.children[0].renderMoreOptionsModal, m.children[0], nameval));

commit f68ec33b27e2b23aa2ab467e634c618e973ac94a

luci-mod-network: make device selector dependent on protocol (again)
This commit effectively reverts the change made with
47c9be91b0 ("luci-mod-network: don't hide "Device" on protocol change").

Floating tunnel protocols such as 6in4, plain PPP over modem device,
VPNC etc. do not have any layer 2 device at all, for such protocols the
device selector should be hidden.

Also swap back the incorrect option order introduced with commit
ed49bbb93f ("luci-mod-network: drop support for *adding* legacy bridges").

Since device depends on proto, it should come after the protocol selection,
not before.

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-mod-network/htdocs/luci-static/resources/view/network/interfaces.js
@@ -503,10 +503,6 @@ return view.extend({
 				}, this);
 				o.write = function() {};
 
-				o = s.taboption('general', widgets.DeviceSelect, 'device', _('Device'));
-				o.nobridges = false;
-				o.optional = false;
-				o.network = ifc.getName();
 
 				proto_select = s.taboption('general', form.ListValue, 'proto', _('Protocol'));
 				proto_select.modalonly = true;
@@ -522,6 +518,11 @@ return view.extend({
 						.then(L.bind(m.render, m))
 						.then(L.bind(this.renderMoreOptionsModal, this, s.section));
 				}, this);
+
+				o = s.taboption('general', widgets.DeviceSelect, 'device', _('Device'));
+				o.nobridges = false;
+				o.optional = false;
+				o.network = ifc.getName();
 
 				o = s.taboption('general', form.Flag, 'auto', _('Bring up on boot'));
 				o.modalonly = true;
@@ -878,7 +879,6 @@ return view.extend({
 					o = s.children[i];
 
 					switch (o.option) {
-					case 'device':
 					case 'proto':
 					case 'auto':
 					case '_dhcp':
@@ -890,6 +890,7 @@ return view.extend({
 					case 'igmp_snooping':
 					case 'stp':
 					case 'type':
+					case 'device':
 						var deps = [];
 						for (var j = 0; j < protocols.length; j++) {
 							if (!protocols[j].isVirtual()) {