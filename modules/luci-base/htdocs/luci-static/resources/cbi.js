slow-boat/luci

commit b8b82179de72a115614c6b7aca6e97d8dd7038ac

luci-base: batch DOM updates to prevent slowdown
Signed-off-by: Olli Asikainen <olli.asikainen@gmail.com>

modules/luci-base/htdocs/luci-static/resources/cbi.js
@@ -752,7 +752,7 @@ function cbi_update_table(table, data, placeholder) {
 		});
 
 		if (Array.isArray(data)) {
-			var n = 0, rows = target.querySelectorAll('tr, .tr');
+			var n = 0, rows = target.querySelectorAll('tr, .tr'), trows = [];
 
 			data.forEach(function(row) {
 				var trow = E('tr', { 'class': 'tr' });
@@ -770,11 +770,15 @@ function cbi_update_table(table, data, placeholder) {
 
 				trow.classList.add('cbi-rowstyle-%d'.format((n++ % 2) ? 2 : 1));
 
-				if (rows[n])
-					target.replaceChild(trow, rows[n]);
-				else
-					target.appendChild(trow);
+				trows[n] = trow;
 			});
+
+			for (var i = 1; i < n; i++) {
+				if (rows[i])
+					target.replaceChild(trows[i], rows[i]);
+				else
+					target.appendChild(trows[i]);
+			}
 
 			while (rows[++n])
 				target.removeChild(rows[n]);

commit 2ae37251fdb378840c1eb3b80805173c4a6599c6

luci-base: fix off-by-one in cbi_update_table()
Fixes: #5189
Fixes: b8b82179de ("luci-base: batch DOM updates to prevent slowdown")
Ref: https://github.com/openwrt/luci/commit/b8b82179de72a115614c6b7aca6e97d8dd7038ac#commitcomment-53495580
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-base/htdocs/luci-static/resources/cbi.js
@@ -773,7 +773,7 @@ function cbi_update_table(table, data, placeholder) {
 				trows[n] = trow;
 			});
 
-			for (var i = 1; i < n; i++) {
+			for (var i = 1; i <= n; i++) {
 				if (rows[i])
 					target.replaceChild(trows[i], rows[i]);
 				else