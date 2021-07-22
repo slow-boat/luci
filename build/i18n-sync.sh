slow-boat/luci

commit b76d16c0579e2b9acfcbaf482d79e28687ae7161

build: i18n-sync.sh: allow restricting to directory
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

build/i18n-sync.sh
@@ -5,9 +5,9 @@
 	exit 1
 }
 
-./build/mkbasepot.sh
+[ -n "$1" ] || ./build/mkbasepot.sh
 
-find . -name '*.pot' -and -not -name base.pot | \
+find "${1:-.}" -name '*.pot' -and -not -name base.pot | \
 	while read path; do
 		dir="${path%/po/templates/*}"
 		echo -n "Updating ${path#./} ... "
@@ -15,4 +15,4 @@ find . -name '*.pot' -and -not -name base.pot | \
 		echo "done"
 	done
 
-./build/i18n-update.pl
+./build/i18n-update.pl "${1:-./*/*/po}"

commit 71a8b097f98ea2d93f9ced6ab898eba70f880ef1

build: i18n-sync.sh: fix i18n-update.pl invocation
Fixes: b76d16c057 ("build: i18n-sync.sh: allow restricting to directory")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

build/i18n-sync.sh
@@ -15,4 +15,10 @@ find "${1:-.}" -name '*.pot' -and -not -name base.pot | \
 		echo "done"
 	done
 
-./build/i18n-update.pl "${1:-./*/*/po}"
+if [ -n "$1" ]; then
+	find "$1" -path '*/templates/*.pot' -printf '%h ' | \
+		xargs -r -n 1 dirname | \
+		xargs -r -n 1 ./build/i18n-update.pl
+else
+	./build/i18n-update.pl
+fi