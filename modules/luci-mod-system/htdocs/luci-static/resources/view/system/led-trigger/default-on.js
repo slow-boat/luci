slow-boat/luci

commit 1a79b161142c619bc0413a2ebad238a264a42d60

luci-mod-system: move default option from defaul-on trigger to none trigger
Signed-off-by: Florian Eckert <fe@dev.tdt.de>

modules/luci-mod-system/htdocs/luci-static/resources/view/system/led-trigger/default-on.js
@@ -8,15 +8,5 @@ return baseclass.extend({
 	kernel: true,
 	addFormOptions(s){
 		var o;
-
-		o = s.option(form.Flag, 'default', _('Default state'));
-		o.rmempty = false;
-		o.depends('trigger', 'default-on');
-		o.textvalue = function(section_id) {
-			var cval = this.cfgvalue(section_id);
-			if (cval == null)
-				cval = this.default;
-			return (cval == this.enabled) ? _('On') : _('Off');
-		};
 	}
 });

modules/luci-mod-system/htdocs/luci-static/resources/view/system/led-trigger/none.js
@@ -7,5 +7,15 @@ return baseclass.extend({
 	kernel: true,
 	addFormOptions(s){
 		var o;
+
+		o = s.option(form.Flag, 'default', _('Default state'));
+		o.rmempty = false;
+		o.depends('trigger', 'none');
+		o.textvalue = function(section_id) {
+			var cval = this.cfgvalue(section_id);
+			if (cval == null)
+				cval = this.default;
+			return (cval == this.enabled) ? _('On') : _('Off');
+		};
 	}
 });

commit ddb5932434aaa3d501ec669947ccec2bc0ec26cd

luci-mod-system: add missing require to `none` led trigger class
Also convert ES6 to ES5 syntax while we're at it.

Fixes: #5476
Fixes: 1a79b16114 ("luci-mod-system: move default option from defaul-on trigger to none trigger")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-mod-system/htdocs/luci-static/resources/view/system/led-trigger/default-on.js
@@ -1,12 +1,9 @@
 'use strict';
 'require baseclass';
-'require form';
 
 return baseclass.extend({
 	trigger: _('Always on (kernel: default-on)'),
 	description: _('The LED is always in default state on.'),
 	kernel: true,
-	addFormOptions(s){
-		var o;
-	}
+	addFormOptions: function(s) {}
 });

modules/luci-mod-system/htdocs/luci-static/resources/view/system/led-trigger/heartbeat.js
@@ -6,7 +6,5 @@ return baseclass.extend({
 	description: _('The LED flashes to simulate actual heart beat.') +
 		_('The frequency is in direct proportion to 1-minute average CPU load.'),
 	kernel: true,
-	addFormOptions(s){
-		var o;
-	}
+	addFormOptions: function(s) {}
 });

modules/luci-mod-system/htdocs/luci-static/resources/view/system/led-trigger/netdev.js
@@ -7,7 +7,7 @@ return baseclass.extend({
 	trigger: _("Network device activity (kernel: netdev)"),
 	description: _('The LED flashes with link status and activity on the configured interface.'),
 	kernel: true,
-	addFormOptions(s){
+	addFormOptions: function(s) {
 		var o;
 
 		o = s.option(widgets.DeviceSelect, '_net_dev', _('Device'));

modules/luci-mod-system/htdocs/luci-static/resources/view/system/led-trigger/none.js
@@ -1,11 +1,12 @@
 'use strict';
+'require form';
 'require baseclass';
 
 return baseclass.extend({
 	trigger: _('Always off (kernel: none)'),
 	description: _('The LED is always in default state off.'),
 	kernel: true,
-	addFormOptions(s){
+	addFormOptions: function(s) {
 		var o;
 
 		o = s.option(form.Flag, 'default', _('Default state'));

modules/luci-mod-system/htdocs/luci-static/resources/view/system/led-trigger/timer.js
@@ -6,7 +6,7 @@ return baseclass.extend({
 	trigger: _('Custom flash interval (kernel: timer)'),
 	description: _('The LED blinks with the configured on/off frequency'),
 	kernel: true,
-	addFormOptions(s){
+	addFormOptions: function(s) {
 		var o;
 
 		o = s.option(form.Value, 'delayon', _('On-State Delay'),