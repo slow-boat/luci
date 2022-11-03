slow-boat/luci

commit ec158e1a4172cd00d7c54f6a8f7239545ca7660b

luci-base: fix luci.http.close()
Ensure that `http.write()` or template rendering operations after a call
to `http.close()` do not produce additional output. This is required for
certain legacy Lua apps which invoke write and close operations in the
middle of a server side cbi rendering process.

Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-base/ucode/dispatcher.uc
@@ -775,7 +775,8 @@ function run_action(request_path, lang, tree, resolved, action) {
 		break;
 
 	case 'call':
-		http.write(render(() => {
+		http.write_headers();
+		http.output(render(() => {
 			runtime.call(action.module, action.function,
 				...(action.parameters ?? []),
 				...resolved.ctx.request_args
@@ -789,7 +790,8 @@ function run_action(request_path, lang, tree, resolved, action) {
 		assert(type(mod[action.function]) == 'function',
 			`Module '${action.module}' does not export function '${action.function}'`);
 
-		http.write(render(() => {
+		http.write_headers();
+		http.output(render(() => {
 			call(mod[action.function], mod, runtime.env,
 				...(action.parameters ?? []),
 				...resolved.ctx.request_args
@@ -798,7 +800,8 @@ function run_action(request_path, lang, tree, resolved, action) {
 		break;
 
 	case 'cbi':
-		http.write(render(() => {
+		http.write_headers();
+		http.output(render(() => {
 			runtime.call('luci.dispatcher', 'invoke_cbi_action',
 				action.path, null,
 				...resolved.ctx.request_args
@@ -807,7 +810,8 @@ function run_action(request_path, lang, tree, resolved, action) {
 		break;
 
 	case 'form':
-		http.write(render(() => {
+		http.write_headers();
+		http.output(render(() => {
 			runtime.call('luci.dispatcher', 'invoke_form_action',
 				action.path,
 				...resolved.ctx.request_args

modules/luci-base/ucode/http.uc
@@ -524,7 +524,7 @@ const Class = {
 
 	// If the content chunk is nil this function will automatically invoke close.
 	write: function(content) {
-		if (content != null) {
+		if (content != null && !this.closed) {
 			this.write_headers();
 			this.output(content);
 

modules/luci-base/ucode/runtime.uc
@@ -48,8 +48,10 @@ const Class = {
 			let bridge = this.env.dispatcher.load_luabridge(optional);
 
 			if (bridge) {
+				let http = this.env.http;
+
 				this.L = bridge.create();
-				this.L.set('L', proto({ write: print }, this.env));
+				this.L.set('L', proto({ write: (...args) => http.closed || print(...args) }, this.env));
 				this.L.invoke('require', 'luci.ucodebridge');
 
 				this.env.lua_active = true;
@@ -61,7 +63,11 @@ const Class = {
 
 	render_ucode: function(path, scope) {
 		let tmplfunc = loadfile(path, { raw_mode: false });
-		call(tmplfunc, null, scope ?? {});
+
+		if (this.env.http.closed)
+			render(call, tmplfunc, null, scope ?? {});
+		else
+			call(tmplfunc, null, scope ?? {});
 	},
 
 	render_lua: function(path, scope) {

commit 3e4ff18089300a255b930c48a353453c16202358

luci-base: dispatcher.uc: only flush HTTP headers after rendering output
Ensure to first completely render the action function before flushing HTTP
headers since the invoked action logic might modify the HTTP headers itself.

Fixes: ec158e1a41 ("luci-base: fix luci.http.close()")
Ref: https://github.com/openwrt/luci/commit/ec158e1a4172cd00d7c54f6a8f7239545ca7660b#commitcomment-88736854
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-base/ucode/dispatcher.uc
@@ -764,6 +764,13 @@ function rollback_pending() {
 
 let dispatch;
 
+function render_action(fn) {
+	const data = render(fn);
+
+	http.write_headers();
+	http.output(data);
+}
+
 function run_action(request_path, lang, tree, resolved, action) {
 	switch (action?.type) {
 	case 'template':
@@ -775,13 +782,12 @@ function run_action(request_path, lang, tree, resolved, action) {
 		break;
 
 	case 'call':
-		http.write_headers();
-		http.output(render(() => {
+		render_action(() => {
 			runtime.call(action.module, action.function,
 				...(action.parameters ?? []),
 				...resolved.ctx.request_args
 			);
-		}));
+		});
 		break;
 
 	case 'function':
@@ -790,33 +796,30 @@ function run_action(request_path, lang, tree, resolved, action) {
 		assert(type(mod[action.function]) == 'function',
 			`Module '${action.module}' does not export function '${action.function}'`);
 
-		http.write_headers();
-		http.output(render(() => {
+		render_action(() => {
 			call(mod[action.function], mod, runtime.env,
 				...(action.parameters ?? []),
 				...resolved.ctx.request_args
 			);
-		}));
+		});
 		break;
 
 	case 'cbi':
-		http.write_headers();
-		http.output(render(() => {
+		render_action(() => {
 			runtime.call('luci.dispatcher', 'invoke_cbi_action',
 				action.path, null,
 				...resolved.ctx.request_args
 			);
-		}));
+		});
 		break;
 
 	case 'form':
-		http.write_headers();
-		http.output(render(() => {
+		render_action(() => {
 			runtime.call('luci.dispatcher', 'invoke_form_action',
 				action.path,
 				...resolved.ctx.request_args
 			);
-		}));
+		});
 		break;
 
 	case 'alias':