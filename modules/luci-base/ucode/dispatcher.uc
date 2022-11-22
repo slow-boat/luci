slow-boat/luci

commit 39574954cf747143f308a6112f636cf37c39e135

luci-base: fix rendering ucode templates from `template` target
A previous commit inadvertently broke support for rendering ucode templates
from the `template` dispatcher target.

Fixes: #6111
Fixes: 793a2bd216 ("luci-base, luci-lua-runtime: adjust Lua template environment")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-base/ucode/dispatcher.uc
@@ -774,9 +774,12 @@ function render_action(fn) {
 function run_action(request_path, lang, tree, resolved, action) {
 	switch (action?.type) {
 	case 'template':
-		render_action(() => {
-			runtime.call('luci.dispatcher', 'render_lua_template', action.path);
-		});
+		if (runtime.is_ucode_template(action.path))
+			runtime.render_ucode(action.path);
+		else
+			render_action(() => {
+				runtime.call('luci.dispatcher', 'render_lua_template', action.path);
+			});
 		break;
 
 	case 'view':

modules/luci-base/ucode/runtime.uc
@@ -61,6 +61,10 @@ const Class = {
 		return this.L;
 	},
 
+	is_ucode_template: function(path) {
+		return access(`${template_directory}/${path}.ut`);
+	},
+
 	render_ucode: function(path, scope) {
 		let tmplfunc = loadfile(path, { raw_mode: false });
 

commit c549e94a6cd89c841e35027c731c760b3734ab42

luci-base: fix (again) rendering ucode templates in `template` target
Fixes: #6111
Fixes: 39574954cf ("luci-base: fix rendering ucode templates from `template` target")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-base/ucode/dispatcher.uc
@@ -775,7 +775,7 @@ function run_action(request_path, lang, tree, resolved, action) {
 	switch (action?.type) {
 	case 'template':
 		if (runtime.is_ucode_template(action.path))
-			runtime.render_ucode(action.path);
+			runtime.render(action.path, {});
 		else
 			render_action(() => {
 				runtime.call('luci.dispatcher', 'render_lua_template', action.path);