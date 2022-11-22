slow-boat/luci

commit 793a2bd2162ef7fe9776f7444c1f652c85873527

luci-base, luci-lua-runtime: adjust Lua template environment
To maintain full compatibility with the old Lua runtime, templates
rendered from a menu `template()` action must implicitly inherit the
`luci.dispatcher` namespace as scope while other indirectly included
templates must not.

Fixes: #6105
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-base/ucode/dispatcher.uc
@@ -774,7 +774,9 @@ function render_action(fn) {
 function run_action(request_path, lang, tree, resolved, action) {
 	switch (action?.type) {
 	case 'template':
-		runtime.render(action.path, {});
+		render_action(() => {
+			runtime.call('luci.dispatcher', 'render_lua_template', action.path);
+		});
 		break;
 
 	case 'view':

modules/luci-lua-runtime/luasrc/dispatcher.lua
@@ -354,6 +354,12 @@ function invoke_form_action(model, ...)
 	_G.L.include("footer")
 end
 
+function render_lua_template(path)
+	local tpl = require "luci.template"
+
+	tpl.render(path, getfenv(1))
+end
+
 
 function call(name, ...)
 	return {

modules/luci-lua-runtime/luasrc/template.lua
@@ -121,7 +121,7 @@ context.viewns = setmetatable({
 	elseif key == "resource" then
 		return L.config.main.resourcebase
 	else
-		return rawget(tbl, key) or disp[key] or _G[key] or L[key]
+		return rawget(tbl, key) or _G[key] or L[key]
 	end
 end})
 

commit 39574954cf747143f308a6112f636cf37c39e135

luci-base: fix rendering ucode templates from `template` target
A previous commit inadvertently broke support for rendering ucode templates
from the `template` dispatcher target.

Fixes: #6111
Fixes: 793a2bd216 ("luci-base, luci-lua-runtime: adjust Lua template environment")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-base/ucode/dispatcher.uc
@@ -774,9 +774,12 @@ function render_action(fn) {
 function run_action(request_path, lang, tree, resolved, action) {
 	switch (action?.type) {
 	case 'template':
-		render_action(() => {
-			runtime.call('luci.dispatcher', 'render_lua_template', action.path);
-		});
+		if (runtime.is_ucode_template(action.path))
+			runtime.render_ucode(action.path);
+		else
+			render_action(() => {
+				runtime.call('luci.dispatcher', 'render_lua_template', action.path);
+			});
 		break;
 
 	case 'view':

modules/luci-base/ucode/runtime.uc
@@ -61,6 +61,10 @@ const Class = {
 		return this.L;
 	},
 
+	is_ucode_template: function(path) {
+		return access(`${template_directory}/${path}.ut`);
+	},
+
 	render_ucode: function(path, scope) {
 		let tmplfunc = loadfile(path, { raw_mode: false });
 