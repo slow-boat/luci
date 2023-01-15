slow-boat/luci

commit ab7bad5a109d14404ee607f051c28d6766c73351

luci-mod-network: Add length restriction to wireless interface name
Based on issue #6101, add a maxlength of 15 chars to the wireless
interface's optional name field. Also validate it as uciname.

Signed-off-by: Hannu Nyman <hannu.nyman@iki.fi>

modules/luci-mod-network/htdocs/luci-static/resources/view/network/wireless.js
@@ -1159,6 +1159,7 @@ return view.extend({
 
 					o = ss.taboption('advanced', form.Value, 'ifname', _('Interface name'), _('Override default interface name'));
 					o.optional = true;
+					o.datatype = 'and(uciname,maxlength(15))';
 					o.placeholder = radioNet.getIfname();
 					if (/^radio\d+\.network/.test(o.placeholder))
 						o.placeholder = '';

commit 3279ba3ed31fa27e3fbb383d7e7462184c507fee

luci-mod-network: remove uciname validation from wireless interface
Remove the 'uciname' validation rule, as it prevents using '-' that
is a valid character.

Fixes: ab7bad5a1 luci-mod-network: Add length restriction to wireless

Signed-off-by: Hannu Nyman <hannu.nyman@iki.fi>

modules/luci-mod-network/htdocs/luci-static/resources/view/network/wireless.js
@@ -1159,7 +1159,7 @@ return view.extend({
 
 					o = ss.taboption('advanced', form.Value, 'ifname', _('Interface name'), _('Override default interface name'));
 					o.optional = true;
-					o.datatype = 'and(uciname,maxlength(15))';
+					o.datatype = 'maxlength(15)';
 					o.placeholder = radioNet.getIfname();
 					if (/^radio\d+\.network/.test(o.placeholder))
 						o.placeholder = '';