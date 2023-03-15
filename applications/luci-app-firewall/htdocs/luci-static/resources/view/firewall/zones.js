slow-boat/luci

commit 5ecfce25e4e874224be8d1841ebd58f2c83bc30e

luci-app-firewall: Add clarification to masquerading option
Signed-off-by: Martijn Staal <27222398+mastaal@users.noreply.github.com>

applications/luci-app-firewall/htdocs/luci-static/resources/view/firewall/zones.js
@@ -156,7 +156,8 @@ return view.extend({
 		p[1].default = fwDefaults.getOutput();
 		p[2].default = fwDefaults.getForward();
 
-		o = s.taboption('general', form.Flag, 'masq', _('Masquerading'));
+		o = s.taboption('general', form.Flag, 'masq', _('Masquerading'),
+			_('Enable network address and port translation (NAT or NAPT) for outbound traffic on this zone. This is typically enabled on the <em>wan</em> zone.'));
 		o.editable = true;
 		o.tooltip = function(section_id) {
 			var masq_src = uci.get('firewall', section_id, 'masq_src')

commit 85da2e0a278991903985b9e35a52a18dd803ad53

luci-app-firewall: suppress lengthy masq description in zone table
Ensure that the description of the masquerade option does not end up in
the grid section overview as it messes up the table layout.

Fixes: 5ecfce25e4 ("luci-app-firewall: Add clarification to masquerading option")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

applications/luci-app-firewall/htdocs/luci-static/resources/view/firewall/zones.js
@@ -95,6 +95,7 @@ return view.extend({
 		s.addremove = true;
 		s.anonymous = true;
 		s.sortable  = true;
+		s.nodescriptions = true;
 
 		s.handleRemove = function(section_id, ev) {
 			return firewall.deleteZone(section_id).then(L.bind(function() {