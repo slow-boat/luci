slow-boat/luci

commit a2519f689d3a6d73e215593238e6dcbe2b5cf19d

luci-mod-network: convert diagnostic page to LuCI look and feel
Signed-off-by: Florian Eckert <fe@dev.tdt.de>

modules/luci-mod-network/htdocs/luci-static/resources/view/network/diagnostics.js
@@ -14,8 +14,7 @@ return view.extend({
 			buttons[i].setAttribute('disabled', 'true');
 
 		return fs.exec(exec, args).then(function(res) {
-			var out = document.querySelector('.command-output');
-			    out.style.display = '';
+			var out = document.querySelector('textarea');
 
 			dom.content(out, [ res.stdout || '', res.stderr || '' ]);
 		}).catch(function(err) {
@@ -75,9 +74,7 @@ return view.extend({
 			ping_host = uci.get('luci', 'diag', 'ping') || 'openwrt.org',
 			route_host = uci.get('luci', 'diag', 'route') || 'openwrt.org';
 
-		return E([], [
-			E('h2', {}, [ _('Network Utilities') ]),
-			E('table', { 'class': 'table' }, [
+		var table = E('table', { 'class': 'table' }, [
 				E('tr', { 'class': 'tr' }, [
 					E('td', { 'class': 'td left' }, [
 						E('input', {
@@ -156,9 +153,26 @@ return view.extend({
 						])
 					]) : E([]),
 				])
-			]),
-			E('pre', { 'class': 'command-output', 'style': 'display:none' })
+			]);
+
+		var view = E('div', { 'class': 'cbi-map'}, [
+			E('h2', {}, [ _('Diagnostics') ]),
+			E('div', { 'class': 'cbi-map-descr'}, _('Execution of various network commands to check the connection and name resolution to other systems.')),
+			table,
+			E('div', {'class': 'cbi-section'}, [
+				E('div', { 'id' : 'command-output'},
+					E('textarea', {
+						'id': 'widget.command-output',
+						'style': 'width: 100%',
+						'readonly': true,
+						'wrap': 'off',
+						'rows': '20'
+					})
+				)
+			])
 		]);
+
+		return view;
 	},
 
 	handleSaveApply: null,

commit ffc7357a2d6cc0861be1b8f4820aeb345e26f43d

luci-mod-network: fix dropdown clipping on diagnostics page
The default overflow hidden style for table cell elements breaks action
dropdowns on the diagnostics page.

Fixes: #6379
Fixes: a2519f689d ("luci-mod-network: convert diagnostic page to LuCI look and feel")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

modules/luci-mod-network/htdocs/luci-static/resources/view/network/diagnostics.js
@@ -76,7 +76,7 @@ return view.extend({
 
 		var table = E('table', { 'class': 'table' }, [
 				E('tr', { 'class': 'tr' }, [
-					E('td', { 'class': 'td left' }, [
+					E('td', { 'class': 'td left', 'style': 'overflow:initial' }, [
 						E('input', {
 							'style': 'margin:5px 0',
 							'type': 'text',
@@ -99,7 +99,7 @@ return view.extend({
 						])
 					]),
 
-					E('td', { 'class': 'td left' }, [
+					E('td', { 'class': 'td left', 'style': 'overflow:initial' }, [
 						E('input', {
 							'style': 'margin:5px 0',
 							'type': 'text',