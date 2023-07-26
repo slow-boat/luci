slow-boat/luci

commit 6ac1292bc6898379db9401546ff574c6ddf38c3e

mosquitto: support notifications_local_only flag
Supported since mosquitto 1.5 released in May 2018, and has long
been supported in the init scripts.

Signed-off-by: Karl Palsson <karlp@etactica.com>

applications/luci-app-mosquitto/luasrc/model/cbi/mosquitto.lua
@@ -161,6 +161,7 @@ OptionalFlag(s, "cleansession", _("Clean session"))
 OptionalFlag(s, "notifications", _("notifications"),
     _("Attempt to notify the local and remote broker of connection status, defaults to $SYS/broker/connections/<clientid>/state"))
 s:option(Value, "notification_topic", _("Topic to use for local+remote remote for notifications.")).optional = true
+OptionalFlag(s, "notification_local_only", _("Notifications local only"), _("Bridge connection states should only be published locally"))
 
 s:option(Value, "remote_clientid", _("Client id to use on remote end of this bridge connection")).optional = true
 s:option(Value, "local_clientid", _("Client id to use locally. Important when bridging to yourself")).optional = true

commit 8d33b26adb0e2b2ebea38595b303ecefd240208a

luci-app-mosquitto: fix notifications_local_only flag
The option wasn't being applied at all because of a typo.

Fixes: 6ac1292bc689 ("mosquitto: support notifications_local_only flag")
Signed-off-by: Sebastian Krzyszkowiak <dos@dosowisko.net>

applications/luci-app-mosquitto/luasrc/model/cbi/mosquitto.lua
@@ -162,7 +162,7 @@ OptionalFlag(s, "cleansession", _("Clean session"))
 OptionalFlag(s, "notifications", _("notifications"),
     _("Attempt to notify the local and remote broker of connection status, defaults to $SYS/broker/connections/<clientid>/state"))
 s:option(Value, "notification_topic", _("Topic to use for local+remote remote for notifications.")).optional = true
-OptionalFlag(s, "notification_local_only", _("Notifications local only"), _("Bridge connection states should only be published locally"))
+OptionalFlag(s, "notifications_local_only", _("Notifications local only"), _("Bridge connection states should only be published locally"))
 
 s:option(Value, "remote_clientid", _("Client id to use on remote end of this bridge connection")).optional = true
 s:option(Value, "local_clientid", _("Client id to use locally. Important when bridging to yourself")).optional = true