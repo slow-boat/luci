slow-boat/luci

commit b1755ae166efd692dfc95e15fa21770acd727591

luci-app-opkg: fix sorting by size column
Fixes: #6120
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

applications/luci-app-opkg/htdocs/luci-static/resources/view/opkg.js
@@ -310,8 +310,9 @@ function display(pattern)
 		currentDisplayRows.push([
 			name,
 			ver,
-			pkg.size ? '%1024mB'.format(pkg.size)
-			         : (altsize ? '~%1024mB'.format(altsize) : '-'),
+			[ pkg.size || 0,
+			   pkg.size ? '%1024mB'.format(pkg.size)
+			         : (altsize ? '~%1024mB'.format(altsize) : '-') ],
 			desc,
 			btn
 		]);

commit 934fa474be4bab988e79f55e994b05fbfbd0e61c

luci-app-opkg: populate alternative package size if needed
Populate the raw size value with the alternative size if the package
size is unavailable.

Fixes: #7139
Fixes: b1755ae166 ("luci-app-opkg: fix sorting by size column")
Signed-off-by: Jo-Philipp Wich <jo@mein.io>

applications/luci-app-opkg/htdocs/luci-static/resources/view/opkg.js
@@ -310,9 +310,9 @@ function display(pattern)
 		currentDisplayRows.push([
 			name,
 			ver,
-			[ pkg.size || 0,
-			   pkg.size ? '%1024mB'.format(pkg.size)
-			         : (altsize ? '~%1024mB'.format(altsize) : '-') ],
+			[ pkg.size || altsize || 0,
+			  pkg.size ? '%1024mB'.format(pkg.size)
+			           : (altsize ? '~%1024mB'.format(altsize) : '-') ],
 			desc,
 			btn
 		]);