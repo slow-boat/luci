slow-boat/luci

commit 8c6e6d8af45114cada3611f053e40a41cfac53f3

build: set i18n-update.pl to break at .po error
Add logic into i18n-update.pl to break on noticing a fatal error
in the .po file. That is needed especially for syncing with
i18n-sync script, where possibly all modules and applications are synced
and errors easily disappear from screen.

Example:

Updating ./applications/luci-app-keepalived/po/de/keepalived.po./applications/luci-app-keepalived/po/de/keepalived.po:928:40: invalid control sequence
./applications/luci-app-keepalived/po/de/keepalived.po:928:51: invalid control sequence
msgmerge: found 2 fatal errors
./build/i18n-update.pl: [] failed: 256

and

Updating modules/luci-base/po/zh_Hant/base.po    modules/luci-base/po/zh_Hant/base.po:1169: duplicate message definition...
modules/luci-base/po/zh_Hant/base.po:1129: ...this is the location of the first definition
modules/luci-base/po/zh_Hant/base.po:1173: duplicate message definition...
modules/luci-base/po/zh_Hant/base.po:1133: ...this is the location of the first definition
msgmerge: found 2 fatal errors
./build/i18n-update.pl: [] failed: 256

Signed-off-by: Hannu Nyman <hannu.nyman@iki.fi>

build/i18n-update.pl
@@ -72,7 +72,7 @@ foreach my $dir (@dirs)
 				my $head = read_header($file);
 
 				printf "Updating %-40s", $file;
-				system("msgmerge", "-U", "-N", $file, "$dir/templates/$basename.pot");
+				system("msgmerge", "-U", "-N", $file, "$dir/templates/$basename.pot") == 0 || die "$0: [$cmd] failed: $?\n";
 
 				write_header($file, $head);
 			}

commit 41018c696214d809c24e1c1f84d013464d580397

build: Fix variable usage in i18n-update.pl
Use 'file' instead of 'cmd' to show the failed file name more clearly.

Fixes: 8c6e6d8 "build: set i18n-update.pl to break at .po error"

Signed-off-by: Hannu Nyman <hannu.nyman@iki.fi>

build/i18n-update.pl
@@ -72,7 +72,7 @@ foreach my $dir (@dirs)
 				my $head = read_header($file);
 
 				printf "Updating %-40s", $file;
-				system("msgmerge", "-U", "-N", $file, "$dir/templates/$basename.pot") == 0 || die "$0: [$cmd] failed: $?\n";
+				system("msgmerge", "-U", "-N", $file, "$dir/templates/$basename.pot") == 0 || die "$0: [$file] failed: $?\n";
 
 				write_header($file, $head);
 			}